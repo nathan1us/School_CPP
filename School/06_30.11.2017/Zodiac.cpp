cf40cce3ee87431114b6ae1a17e3c5623a45b9a3
slaweykow <slaweykow@gmail.com>
2018-01-17 01:40:48 +0200

    Pushing some old stuff.

---
+++ School/06_30.11.2017/Zodiac.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int month, day;
+	cout << "Enter month: ";
+	cin >> month;
+	cout << "Enter day: ";
+	cin >> day;
+
+	if (month == 1)
+		cout << "January" << endl;
+
+	system("pause");
+	return 0;
+}
